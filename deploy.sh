a0127640e7214863d9b8494a15d45e4eb656951f
wishnekd <don.wishnek@aciworldwide.com>
2022-09-13 17:29:08 -0400

    modified travis

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t donwishnek/multi-client-k8s:latest -t donwishnek/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t donwishnek/multi-server-k8s-pgfix:latest -t donwishnek/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t donwishnek/multi-worker-k8s:latest -t donwishnek/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t donwishnek/multi-client:latest -t donwishnek/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t donwishnek/multi-server:latest -t donwishnek/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t donwishnek/multi-worker:latest -t donwishnek/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push donwishnek/multi-client-k8s:latest
-docker push donwishnek/multi-server-k8s-pgfix:latest
-docker push donwishnek/multi-worker-k8s:latest
+docker push donwishnek/multi-client:latest
+docker push donwishnek/multi-server:latest
+docker push donwishnek/multi-worker:latest
 
-docker push donwishnek/multi-client-k8s:$SHA
-docker push donwishnek/multi-server-k8s-pgfix:$SHA
-docker push donwishnek/multi-worker-k8s:$SHA
+docker push donwishnek/multi-client:$SHA
+docker push donwishnek/multi-server:$SHA
+docker push donwishnek/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=donwishnek/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=donwishnek/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=donwishnek/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=donwishnek/multi-server:$SHA
+kubectl set image deployments/client-deployment client=donwishnek/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=donwishnek/multi-worker:$SHA
